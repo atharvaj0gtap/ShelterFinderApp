3707f0067ac12484564dcddb7ba1967d6211b688
Atharva Jagtap <atharvauni2021@gmail.com>
2023-12-04 16:59:50 -0800

    Update atharva

--- app/src/main/java/com/example/app/ViewBooking.java
+++ app/src/main/java/com/example/app/ViewBooking.java
@@ -3,24 +3,37 @@ package com.example.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class ViewBooking extends AppCompatActivity {
-    EditText fromdate, todate;
+    EditText fromdate, todate, occupancy, comments;
     private int fYear, fMonth, fDay, tYear, tMonth, tDay;
+    TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_booking);
 
+        Intent intent = getIntent();
+        //username = findViewById(R.id.usernameView1);
+        //username.setText(intent.getStringExtra("username"));
+
         fromdate = (EditText) findViewById(R.id.fromdateInp);
         todate = (EditText) findViewById(R.id.todateInp);
+        occupancy = (EditText) findViewById(R.id.noInp);
+        comments = (EditText) findViewById(R.id.detailsInp);
     }
 
     public void setFromDate(View v){
@@ -66,7 +79,55 @@ public class ViewBooking extends AppCompatActivity {
     }
 
     public void onBack(View v){
-
+        finish();
     }
 
+    public void onBook(View v){
+        if (!isValidDates())
+            Toast.makeText(this, "Please enter a valid to and/or from date", Toast.LENGTH_SHORT).show();
+        else if(!isValidOccupancy())
+            Toast.makeText(this, "Please enter a valid Occupancy (Hint: Integer > 0 )", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Booked successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isValidOccupancy() {
+        String occupancyText = occupancy.getText().toString().trim();
+        if (occupancyText.isEmpty()) {
+            return false;
+        }
+
+        try {
+            int occupancy = Integer.parseInt(occupancyText);
+            return occupancy > 0; //&& occupancy < maxOccupancy;   // Make sure to edit this once max is known
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isValidDates() {
+        //Make sure to enter dates between the given dates for listing
+
+        String fromDateText = fromdate.getText().toString().trim();
+        String toDateText = todate.getText().toString().trim();
+
+        if (fromDateText.isEmpty() || toDateText.isEmpty()) {
+            return false;
+        }
+
+        Date fromDate = parseDate(fromDateText);
+        Date toDate = parseDate(toDateText);
+
+        return toDate.after(fromDate);
+    }
+
+    private Date parseDate(String dateString) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            return dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- app/src/main/java/com/example/app/ViewReviews.java
+++ app/src/main/java/com/example/app/ViewReviews.java
@@ -2,16 +2,24 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ViewReviews extends AppCompatActivity {
     RatingBar reviewRating;
     double myRating = 0;
+    TextView username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_reviews);
+
+        Intent intent = getIntent();
+        username = findViewById(R.id.usernameView);
+        //username.setText(intent.getStringExtra("username"));
 
         reviewRating = findViewById(R.id.reviewRating);
 
@@ -21,5 +29,9 @@ public class ViewReviews extends AppCompatActivity {
                 myRating = ratingBar.getRating();
             }
         });
+    }
+
+    public void onBack(View v){
+        finish();
     }
 }
